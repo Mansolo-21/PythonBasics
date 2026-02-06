d21f4b88d9d4ba6bfd17199e74a83032ebee0a4b
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-06 15:50:21 +0300

    r

--- dictionaries.py
+++ dictionaries.py
@@ -39,6 +39,37 @@ print(student)
 print(type(student))
 
 #accessing dictionary items
-print(student["student name"])
+print(student["Student name"])
 print(student["age"])
 print(student["course"])
+
+#adding key:value pair
+student["grade"]="A"
+print(student)
+
+#updating a value
+student["course"]="Web Development"
+print(student)
+
+#accessing all keys called keys
+print(student.keys())
+
+#accessing all values, values()
+print(student.values())
+
+#accessing all keys and values
+print(student.items())
+
+#loop thru all keys
+for x in student.keys():
+    print(x)
+    
+
+#loop thru all values
+for y in student.values():
+    print(y)
+    
+
+#loop thru all items
+for z in student.items():
+    print(z)
