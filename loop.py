5e403e4138b993560a19817f031caa6ec202a1cd
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-06 16:17:50 +0300

    r

---
+++ loop.py
@@ -0,0 +1,25 @@
+#for loop-for itreating through a sequence
+
+#looping through list
+fruits=["oranges","apples","bananas","mangoes"]
+for fruit in fruits:
+    print(fruit)
+    
+    
+#looping through a string
+for x in "Hello":
+    print(x)
+    
+#looping through a range
+range(2,6)
+
+for z in range(2,6):
+    print(z) 
+
+#loop 
+for b in range(9):
+    print((b))
+    
+for e in range(0,30,2):
+    print(e)
+    
