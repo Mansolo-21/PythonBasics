fbd49da9c53abd9b0c0fdd2903e2bb6014710a51
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-10 16:06:45 +0300

    python

--- try.py
+++ try.py
@@ -5,17 +5,19 @@ except
     code that runs if error happens
     
 """
-try:
-    num=int(input("Enter a number, "))
-    print(10/num)
-except:
-    print("You cannot divide a number by 0")
+# try:
+#     num=int(input("Enter a number, "))
+#     print(10/num)
+# except:
+#     print("You cannot divide a number by 0")
     
-#example
-try:
-    print()    
-except NameError:
-    print("The variable is not defined")
-    
-with open('wawr.txt','r')as x:
-    print(x)
+# #example
+# try:
+#     print()    
+# except NameError:
+#     print("The variable is not defined")
+try:    
+    with open('wawr.txt','r')as x:
+        print(x)
+except FileNotFoundError :
+    print("The file does not exist")
