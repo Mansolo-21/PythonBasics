7766cb10e177fc41cad4cb046140fe6af8c29c1c
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-10 15:06:52 +0300

    python

--- myscript.py
+++ myscript.py
@@ -1,3 +1,9 @@
-import mymodule
-print(mymodule.addTwoNumbers(34,34))
-print(mymodule.display_info("Jayden","Python"))
+# import mymodule
+# print(mymodule.addTwoNumbers(34,34))
+# # print(mymodule.display_info("Jayden","Python"))
+# from mymodule import addTwoNumbers,display_info
+# print(display_info("Kageno","Full stack"))
+# print(addTwoNumbers(34,27))
+import mymodule as mystuff
+print(mystuff.addTwoNumbers(13,34))
+print(mystuff.display_info("Henry Danger","Sidekicking")) 
